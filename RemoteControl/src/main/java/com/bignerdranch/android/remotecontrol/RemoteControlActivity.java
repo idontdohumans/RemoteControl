7324771b2b15547a5a02e339dc46fc310bc0a89d
Donald <donald@thehoefer.com>
2013-07-08 08:18:27 -0400

    Initial commit

---
+++ RemoteControl/src/main/java/com/bignerdranch/android/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.remotecontrol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RemoteControlActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.remote_control, menu);
+        return true;
+    }
+    
+}
